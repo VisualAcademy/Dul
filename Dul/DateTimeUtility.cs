44c5c0d9d56b05454a34ed443499616a70bea9aa
WebCampKR <webcamp@outlook.kr>
2018-12-15 21:29:24 +0900

    주요 유틸리티 및 라이브러리 클래스 추가

---
+++ Dul/DateTimeUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dul
+{
+    /// <summary>
+    /// 날짜와 시간 관련 유틸리티 
+    /// </summary>
+    public class DateTimeUtility
+    {
+        /// <summary>
+        /// 1부터 8760까지의 정수를 입력받아 해당 날짜를 반환해주는 함수 
+        /// </summary>
+        public static DateTime GetDateTimeFromYearlyHourNumber(int number)
+        {
+            return (new DateTime(2019, 1, 1, 0, 0, 0)).AddHours(--number);
+        }
+    }
+}
