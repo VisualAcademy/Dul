c1f719f9e8ad45f602505308c7c252f1d24c6181
RedPlus <redplus@redplus.net>
2019-07-29 14:15:59 +0900

    Dul.Math.Abs() 집필 코드 추가

---
+++ Dul.Tests/00_Assert/AssertDemo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dul.Tests
+{
+    [TestClass]
+    public class AssertDemo
+    {
+        [TestMethod]
+        public void AssertAreEqualTest()
+        {
+            Assert.AreEqual(1234, 1234);
+        }
+    }
+}
