cd099ab750eaf895cbce4356e9ee5108e8bf0353
RedPlus <redplus@redplus.net>
2019-08-19 09:14:26 +0900

    코드 정리

--- Dul/StringLibrary.cs
+++ Dul/StringLibrary.cs
@@ -5,16 +5,17 @@
         /// <summary>
         /// 주어진 문자열을 주어진 길이만큼만 잘라서 반환, 나머지 부분은 '...'을 붙임
         /// </summary>
-        /// <param name="strCut">원본 문자열</param>
-        /// <param name="intChar">잘라낼 길이</param>
+        /// <param name="cut">원본 문자열</param>
+        /// <param name="length">잘라낼 길이</param>
         /// <returns>안녕하세요. => 안녕...</returns>
-        public static string CutString(this string strCut, int intChar, string suffix = "...")
+        public static string CutString(
+            this string cut, int length, string suffix = "...")
         {
-            if (strCut.Length > (intChar - 3))
+            if (cut.Length > (length - 3))
             {
-                return strCut.Substring(0, intChar - 3) + "" + suffix;
+                return cut.Substring(0, length - 3) + "" + suffix;
             }
-            return strCut;
+            return cut;
         }
 
         /// <summary>
@@ -22,8 +23,9 @@
         /// </summary>
         /// <param name="str">한글, 영문, 유니코드 문자열</param>
         /// <param name="length">자를 문자열의 길이</param>
-        /// <returns>잘라진 문자열</returns>
-        public static string CutStringUnicode(this string str, int length, string suffix = "...")
+        /// <returns>잘라진 문자열: 안녕하세요. => 안녕...</returns>
+        public static string CutStringUnicode(
+            this string str, int length, string suffix = "...")
         {
             string result = str;
 
