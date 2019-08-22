a317cdd24cdb4662e125c92ff65667648140036f
RedPlus <redplus@redplus.net>
2019-08-23 01:37:59 +0900

    코드 정리

--- Dul/04_DateTime/DateTimeUtility.cs
+++ Dul/04_DateTime/DateTimeUtility.cs
@@ -9,26 +9,6 @@ namespace Dul
     public class DateTimeUtility
     {
         /// <summary>
-        /// 1부터 8760까지의 정수를 입력받아 해당 날짜를 반환해주는 함수 
-        /// </summary>
-        public static DateTime GetDateTimeFromYearlyHourNumber(int number)
-        {
-            return (new DateTime(2019, 1, 1, 0, 0, 0)).AddHours(--number);
-        }
-
-        /// <summary>
-        /// 특정 월/일/시에 대한 년간 시간 값을 반환: 8760 시간만 다루기 
-        /// </summary>
-        public static int GetYearlyHourByDateTime(int month, int day, int hour)
-        {
-            int yearlyHour = 0;
-
-            yearlyHour = (int)(new DateTime(2019, month, day, hour, 0, 0) - new DateTime(2019, 1, 1, 0, 0, 0)).TotalHours;
-
-            return ++yearlyHour; 
-        }
-        
-        /// <summary>
         /// 날짜 형식이 오늘 날짜면 시간 표시 다르면 날짜 표시
         /// </summary>
         public static string ShowTimeOrDate(object dt, string format = "yyyy-MM-dd")
@@ -49,6 +29,26 @@ namespace Dul
                 return "";
             }
         }
+
+        /// <summary>
+        /// 1부터 8760까지의 정수를 입력받아 해당 날짜를 반환해주는 함수 
+        /// </summary>
+        public static DateTime GetDateTimeFromYearlyHourNumber(int number)
+        {
+            return (new DateTime(2019, 1, 1, 0, 0, 0)).AddHours(--number);
+        }
+
+        /// <summary>
+        /// 특정 월/일/시에 대한 년간 시간 값을 반환: 8760 시간만 다루기 
+        /// </summary>
+        public static int GetYearlyHourByDateTime(int month, int day, int hour)
+        {
+            int yearlyHour = 0;
+
+            yearlyHour = (int)(new DateTime(2019, month, day, hour, 0, 0) - new DateTime(2019, 1, 1, 0, 0, 0)).TotalHours;
+
+            return ++yearlyHour; 
+        }        
 
         /// <summary>
         /// 1부터 12까지의 키 값을 갖는 빈 컬렉션 반환
