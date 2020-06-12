1277f817ded2f98d7752b6424000ce4b07c3595e
RedPlus <redplus@redplus.net>
2020-06-13 07:24:40 +0900

    ICrudRepositoryBase 추가 후 1.2.4로 버전 업데이트

---
+++ Dul/07_Articles/ICrudRepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dul.Articles
+{
+    /// <summary>
+    /// CRUD 제네릭 인터페이스
+    /// </summary>
+    /// <typeparam name="T">모델 클래스 형식</typeparam>
+    /// <typeparam name="TIdentifier">식별자 형식</typeparam>
+    public interface ICrudRepositoryBase<T, TIdentifier>
+    {
+        Task<T> AddAsync(T model); // 입력
+        Task<List<T>> GetAllAsync(); // 출력
+        Task<T> GetByIdAsync(TIdentifier id); // 상세
+        Task<bool> UpdateAsync(T model); // 수정
+        Task<bool> DeleteAsync(TIdentifier id); // 삭제
+        // 필터링 
+        Task<ArticleSet<T, int>> GetArticlesAsync<TParentIdentifier>(
+            int pageIndex,
+            int pageSize,
+            string searchField,
+            string searchQuery,
+            string sortOrder,
+            TParentIdentifier parentIdentifier);
+    }
+}
