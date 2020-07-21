c34aadc235c72811bfd4f809e2b95b4318bc4590
RedPlus <redplus@redplus.net>
2020-07-22 04:16:22 +0900

    강의용 유틸리티 데모용 클래스 추가

--- Dul/07_Articles/ICrudRepositoryBase.cs
+++ Dul/07_Articles/ICrudRepositoryBase.cs
@@ -7,8 +7,8 @@ namespace Dul.Articles
     /// CRUD 제네릭 인터페이스
     /// </summary>
     /// <typeparam name="T">모델 클래스 형식</typeparam>
-    /// <typeparam name="TIdentifier">식별자 형식</typeparam>
-    public interface ICrudRepositoryBase<T, TIdentifier> where T : class 
+    /// <typeparam name="TIdentifier">부모 식별자 형식</typeparam>
+    public interface ICrudRepositoryBase<T, TIdentifier> where T : class
     {
         Task<T> AddAsync(T model); // 입력
         Task<List<T>> GetAllAsync(); // 출력
@@ -17,6 +17,29 @@ namespace Dul.Articles
         Task<bool> DeleteAsync(TIdentifier id); // 삭제
         // 필터링 
         Task<ArticleSet<T, int>> GetArticlesAsync<TParentIdentifier>(
+            int pageIndex,
+            int pageSize,
+            string searchField,
+            string searchQuery,
+            string sortOrder,
+            TParentIdentifier parentIdentifier);
+    }
+
+    /// <summary>
+    /// CRUD 제네릭 인터페이스
+    /// </summary>
+    /// <typeparam name="T">모델 클래스 형식</typeparam>
+    /// <typeparam name="V">Id 형식: int or long</typeparam>
+    /// <typeparam name="TIdentifier">부모 식별자 형식</typeparam>
+    public interface ICrudRepositoryBase<T, V, TIdentifier> where T : class
+    {
+        Task<T> AddAsync(T model); // 입력
+        Task<List<T>> GetAllAsync(); // 출력
+        Task<T> GetByIdAsync(TIdentifier id); // 상세
+        Task<bool> UpdateAsync(T model); // 수정
+        Task<bool> DeleteAsync(TIdentifier id); // 삭제
+        // 필터링 
+        Task<ArticleSet<T, V>> GetArticlesAsync<TParentIdentifier>(
             int pageIndex,
             int pageSize,
             string searchField,
---
+++ Dul/12_Utilities/TemperatureConverter.cs
@@ -0,0 +1,34 @@
+﻿// 참고: https://docs.microsoft.com/ko-kr/dotnet/csharp/programming-guide/classes-and-structs/static-classes-and-static-class-members
+using System;
+
+namespace Dul.Utilities
+{
+    public static partial class TemperatureConverter
+    {
+        public static string Version = "1.0";
+        public static double CelsiusToFahrenheit(string temperatureCelsius)
+        {
+            // Convert argument to double for calculations.
+            double celsius = Double.Parse(temperatureCelsius);
+
+            // Convert Celsius to Fahrenheit.
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            return fahrenheit;
+        }
+    }
+
+    public static partial class TemperatureConverter
+    {
+        public static double FahrenheitToCelsius(string temperatureFahrenheit)
+        {
+            // Convert argument to double for calculations.
+            double fahrenheit = Double.Parse(temperatureFahrenheit);
+
+            // Convert Fahrenheit to Celsius.
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+    }
+}
