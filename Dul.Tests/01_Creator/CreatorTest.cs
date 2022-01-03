571b718a2b6f6633e8abd0308a2c9de0ae5bddaf
Park YongJune <redplus@redplus.net>
2022-01-03 22:38:00 +0900

    Code Cleanup

--- Dul.Tests/01_Creator/CreatorTest.cs
+++ Dul.Tests/01_Creator/CreatorTest.cs
@@ -19,3 +19,30 @@ namespace Dul.Tests
         }
     }
 }
+
+namespace Dul
+{
+    class CreatorOther
+    {
+        public static string GetName() => "RedPlus";
+    }
+    namespace Tests
+    {
+        [TestClass]
+        public class CreatorTestOther
+        {
+            [TestMethod]
+            public void GetNameTest()
+            {
+                //[1] Arrange(정렬), Setup
+                const string expected = "RedPlus";
+
+                //[2] Act(동작), Execute
+                var actual = CreatorOther.GetName();
+
+                //[3] Assert(어설션), Verify
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
