c6df6ea2d406cf25446d80e12a13a2398707ba10
Park YongJune <redplus@redplus.net>
2024-03-24 11:52:04 +0900

    File Cleanup

--- Dul.App/Program.cs
+++ Dul.App/Program.cs
@@ -14,6 +14,18 @@ namespace Dul.App
 
             //[3] StringLibrary 호출 테스트
             Console.WriteLine("안녕하세요.".CutStringUnicode(6));
+
+            //[1] Creator 호출 테스트
+            Console.WriteLine(Creator.GetName());
+
+            //[2] Math 호출 테스트
+            Console.WriteLine(Dul.Math.Abs(-1234)); // System.Math와 Dul.Math 충돌 방지
+
+            //[3] StringLibrary 호출 테스트
+            Console.WriteLine("안녕하세요.".CutStringUnicode(6));
+
+            //[4] DateTimeUtility 호출 테스트
+            Console.WriteLine(DateTimeUtility.ShowTimeOrDate(DateTime.Now));
         }
     }
 }
