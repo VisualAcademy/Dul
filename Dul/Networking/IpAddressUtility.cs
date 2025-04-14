b8b8bdef4393d0e8e10ee1d2a4b10cbf0a2fcd1d
Park YongJune <redplus@redplus.net>
2025-04-15 01:53:25 +0900

    Add IpAddressUtility and PhoneNumberFormatter class

---
+++ Dul/Networking/IpAddressUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace Dul.Networking
+{
+    /// <summary>
+    /// Provides utility methods for IP address operations.
+    /// </summary>
+    public static class IpAddressUtility
+    {
+        /// <summary>
+        /// Determines whether the specified IP address is within the given range.
+        /// </summary>
+        /// <param name="currentIp">The IP address to check.</param>
+        /// <param name="startIp">The start of the IP range.</param>
+        /// <param name="endIp">The end of the IP range.</param>
+        /// <returns><c>true</c> if the IP address is within range; otherwise, <c>false</c>.</returns>
+        public static bool IsIpInRange(string currentIp, string startIp, string endIp)
+        {
+            var addr = IPAddress.Parse(currentIp);
+            var lowerBound = IPAddress.Parse(startIp);
+            var upperBound = IPAddress.Parse(endIp);
+
+            byte[] addrBytes = addr.GetAddressBytes();
+            byte[] lowerBytes = lowerBound.GetAddressBytes();
+            byte[] upperBytes = upperBound.GetAddressBytes();
+
+            for (int i = 0; i < addrBytes.Length; i++)
+            {
+                if (addrBytes[i] < lowerBytes[i] || addrBytes[i] > upperBytes[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Dul/Utilities/Formatters/PhoneNumberFormatter.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Dul.Utilities.Formatters
+{
+    public static class PhoneNumberFormatter
+    {
+        public static string Format(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                return "None";
+
+            var digitsOnly = new string(phoneNumber.Where(char.IsDigit).ToArray());
+
+            // 미국/캐나다 번호 (10자리)
+            if (digitsOnly.Length == 10 && digitsOnly.StartsWith("1") == false && digitsOnly.StartsWith("0") == false)
+            {
+                return $"({digitsOnly.Substring(0, 3)}) {digitsOnly.Substring(3, 3)}-{digitsOnly.Substring(6, 4)}";
+            }
+
+            // 한국 휴대폰 번호 (11자리, 010으로 시작)
+            if (digitsOnly.Length == 11 && digitsOnly.StartsWith("010"))
+            {
+                return $"{digitsOnly.Substring(0, 3)}-{digitsOnly.Substring(3, 4)}-{digitsOnly.Substring(7, 4)}";
+            }
+
+            // 한국 서울 번호 (10자리, 02로 시작)
+            if (digitsOnly.Length == 10 && digitsOnly.StartsWith("02"))
+            {
+                return $"{digitsOnly.Substring(0, 2)}-{digitsOnly.Substring(2, 4)}-{digitsOnly.Substring(6, 4)}";
+            }
+
+            // 한국 기타 지역번호 (10자리, 03X, 04X, 05X 등)
+            if (digitsOnly.Length == 10 && digitsOnly.StartsWith("0"))
+            {
+                return $"{digitsOnly.Substring(0, 3)}-{digitsOnly.Substring(3, 3)}-{digitsOnly.Substring(6, 4)}";
+            }
+
+            // 그 외: 원본 반환
+            return phoneNumber;
+        }
+    }
+}
